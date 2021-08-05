582306193eacb2db9a676e1f8c5351e83d9b2136
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2021-08-05 22:30:34 +1200

    Switch -Werror back on

--- nix/pkgs/haskell/haskell.nix
+++ nix/pkgs/haskell/haskell.nix
@@ -234,7 +234,7 @@ let
           plutus-playground-server.ghcOptions = [ "-Werror" ];
           plutus-pab.ghcOptions = [ "-Werror" ];
           plutus-tx.ghcOptions = [ "-Werror" ];
-          plutus-tx-plugin.ghcOptions = lib.optional (!pkgs.stdenv.hostPlatform.isWindows) "-Werror";
+          plutus-tx-plugin.ghcOptions = [ "-Werror" ];
           plutus-doc.ghcOptions = [ "-Werror" ];
           plutus-use-cases.ghcOptions = [ "-Werror" ];
 
